c28ede90ffa8a8ac86f24961a2ea8c45fddce675
Ahmed Habib <73848497+AhmedHabib00@users.noreply.github.com>
2023-03-15 21:46:58 +0200

    init be, add docker

---
+++ backend/src/main/java/com/wecare/gp/GpApplication.java
@@ -0,0 +1,13 @@
+package com.wecare.gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/wecare/gp/GpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecare.gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

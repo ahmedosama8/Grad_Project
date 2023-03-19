227eb073532f6fc4cb2769b398e171e1c0e560aa
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2023-03-19 19:00:42 +0200

    login page initialized

---
+++ mobile_app/lib/pages/login.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_template/flutter_login_template.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Login> {
+  bool _ishiddenpassword = true;
+  final password = TextEditingController();
+  final email = TextEditingController();
+  bool emailfilled = false;
+  bool passwordfilled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 150),
+                Center(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage('assets/lotus.png'),
+                    radius: 40.0,
+                  ),
+                ),
+                SizedBox(height: 50),
+                TextField(
+                    controller: email,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        emailfilled = value.length >= 1 ? true : false;
+                      });
+                    }),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                    controller: password,
+                    obscureText: _ishiddenpassword,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: toggleIcon,
+                        child: Icon(
+                          _ishiddenpassword
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                      ),
+                      labelText: 'Password',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        passwordfilled = value.length >= 1 ? true : false;
+                      });
+                    }),
+                SizedBox(height: 20.0),
+                Center(
+                  child: SizedBox(
+                    height: 50, //height of button
+                    width: 150,
+                    child: ElevatedButton(
+                      child: Text("log in ",
+                          style: TextStyle(color: Colors.white)),
+                      onPressed: () {
+                        if (passwordfilled == false && emailfilled == false) {
+                          _onBasicAlertPressed(context);
+                        }
+                      },
+                    ),
+                  ),
+                )
+              ],
+            )));
+  }
+
+  void toggleIcon() {
+    setState(() {
+      _ishiddenpassword = !_ishiddenpassword;
+    });
+  }
+
+  _onBasicAlertPressed(context) {
+    Alert(
+      context: context,
+      title: "Log-in Error",
+      desc: "please fill the data to log in ",
+    ).show();
+  }
+}

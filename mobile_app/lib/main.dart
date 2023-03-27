549b7235033e2de08b224e71ce016a2e4e0e6b90
ahmedosama8 <ahmedosamaa1234@gmail.com>
2023-03-27 16:46:23 +0200

    home page added

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_template/flutter_login_template.dart';
+import 'package:mobile_app/pages/home.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'pages/login.dart';
 import 'pages/signup.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => Login(),
+        '/': (context) => HomePage(),
+        '/login': (context) => Login(),
         '/signup': (context) => signup(),
       },
     ));

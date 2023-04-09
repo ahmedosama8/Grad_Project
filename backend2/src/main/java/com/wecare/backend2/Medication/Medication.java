a7cbad9c781aa732d3e83a4cbc6b66cd8edb62f4
Ahmed Habib <73848497+AhmedHabib00@users.noreply.github.com>
2023-04-10 01:54:21 +0200

    add meds methods

--- backend2/src/main/java/com/wecare/backend2/Medication/Medication.java
+++ backend2/src/main/java/com/wecare/backend2/Medication/Medication.java
@@ -29,5 +29,77 @@ public class Medication {
     private LocalDate startDate;
     private LocalDate endDate;
 
+    public Medication() {
+    }
 
+    public Medication(Long medicationId, Patient patient, Diagnosis diagnosis, String medicationName, String medicationDose, String instructions, LocalDate startDate, LocalDate endDate) {
+        this.medicationId = medicationId;
+        this.patient = patient;
+        this.diagnosis = diagnosis;
+        this.medicationName = medicationName;
+        this.medicationDose = medicationDose;
+        this.instructions = instructions;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getMedicationId() {
+        return medicationId;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Diagnosis getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(Diagnosis diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public String getMedicationDose() {
+        return medicationDose;
+    }
+
+    public void setMedicationDose(String medicationDose) {
+        this.medicationDose = medicationDose;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }

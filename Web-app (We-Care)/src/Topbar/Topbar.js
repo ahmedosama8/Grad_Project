f48e854be5bb39db4756552b49c50761cb966f80
MahmoudFawzy4444 <55939077+MahmoudFawzy4444@users.noreply.github.com>
2023-04-17 15:19:43 +0200

    some tests

--- Web-app (We-Care)/src/Topbar/Topbar.js
+++ Web-app (We-Care)/src/Topbar/Topbar.js
@@ -7,13 +7,17 @@ export default function Topbar() {
     <div className="topbar" id="AdminTopBar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">We-Care</span>
+          <span className="logo">Self Care</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
             {/* <NotificationsNone  /> */}
           </div>
-          <img src="https://cdn-icons-png.flaticon.com/512/9379/9379978.png" alt="" className="topAvatar" />
+          <img
+            src="https://media.discordapp.net/attachments/795560684120834061/1090738184377348216/loggo.png?width=146&height=142"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>

471c084c63567430b74ac861d281b03e3f5b06a1
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2023-04-17 13:26:33 +0200

    scrollable menus

--- mobile_app/lib/pages/doctor_report.dart
+++ mobile_app/lib/pages/doctor_report.dart
@@ -20,68 +20,75 @@ class _dr_reportState extends State<dr_report> {
       ),
       body: Padding(
         padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      radius: 20,
-                      backgroundImage: AssetImage('assets/doctor.png'),
-                    ),
-                    title: const Text('Doctor: name'),
-                  ),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.medication),
-                    title: Text('medications'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'panadol,adol,moov',
-                        style: TextStyle(
-                            color: Colors.black.withOpacity(0.6), fontSize: 20),
+        child: ListView(
+          controller: ScrollController(),
+          children: [
+            Column(
+              children: <Widget>[
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          radius: 20,
+                          backgroundImage: AssetImage('assets/doctor.png'),
+                        ),
+                        title: const Text('Doctor: name'),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.note),
-                    title: Text('Report'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Patients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomy.',
-                        style: TextStyle(
-                            color: Colors.black.withOpacity(0.6), fontSize: 20),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.medication),
+                        title: Text('medications'),
                       ),
-                    ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'panadol,adol,moov',
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.6),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.note),
+                        title: Text('Report'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Patients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomy.Patients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomy',
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.6),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- mobile_app/lib/pages/rad_report.dart
+++ mobile_app/lib/pages/rad_report.dart
@@ -20,57 +20,63 @@ class _rad_reportState extends State<rad_report> {
       ),
       body: Padding(
         padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.local_hospital),
-                    title: const Text('Radiology Center: name'),
-                  ),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.note),
-                    title: Text('Report'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Patients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomy.',
-                        style: TextStyle(
-                            color: Colors.black.withOpacity(0.6), fontSize: 20),
+        child: ListView(
+          controller: ScrollController(),
+          children: [
+            Column(
+              children: <Widget>[
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.local_hospital),
+                        title: const Text('Radiology Center: name'),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              shadowColor: Colors.greenAccent,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      radius: 20,
-                      backgroundImage: AssetImage('assets/doctor.png'),
-                    ),
-                    title: const Text('perfromed by: name'),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.note),
+                        title: Text('Report'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Patients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomyPatients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomyPatients may require portable suction machines in their home if they are unable to clear their own secretions for a variety of reasons. This includes patients who are receiving palliative care and find it difficult or impossible to clear their own secretions, individuals with chronic illnesses (COPD, ALS, cystic fibrosis, bronchiectasis, etc.), or patients who have undergone a tracheostomy.',
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.6),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  shadowColor: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          radius: 20,
+                          backgroundImage: AssetImage('assets/doctor.png'),
+                        ),
+                        title: const Text('perfromed by: name'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),

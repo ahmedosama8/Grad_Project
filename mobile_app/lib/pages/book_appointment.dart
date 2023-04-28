e3e2f359f97fa1babe94d95ad2a82ad2f4b87eb4
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2023-04-28 17:14:47 +0300

    Update book_appointment.dart

--- mobile_app/lib/pages/book_appointment.dart
+++ mobile_app/lib/pages/book_appointment.dart
@@ -15,7 +15,6 @@ class _BookappointState extends State<Bookappoint> {
   final _nameController = TextEditingController();
   final _notesController = TextEditingController();
   TextEditingController dateinput = TextEditingController();
-  //final _dateController = TextEditingController();
   List<String> facilities = ['Dr.Ga3fr el 3omda', 'Nile Scan', 'Alfa lab'];
   String? selectedFacility;
   void facilityCallBack(String? selectedFacVal) {
@@ -76,100 +75,102 @@ class _BookappointState extends State<Bookappoint> {
       ),
       body: Form(
         key: _formKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-                controller: _nameController,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 2, color: Colors.greenAccent),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Full Name',
-                    prefixIcon: Icon(Icons.person_2_outlined)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: SizedBox(
-                width: double.infinity,
-                child: DropdownButtonFormField(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                  controller: _nameController,
                   decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.local_hospital_outlined),
                       enabledBorder: OutlineInputBorder(
                         borderSide:
                             BorderSide(width: 2, color: Colors.greenAccent),
                         borderRadius: BorderRadius.circular(12),
-                      )),
-                  hint: Text('Facility'),
-                  isExpanded: true,
-                  items: facilities
-                      .map<DropdownMenuItem<String>>((String selectedFacVal) {
-                    return DropdownMenuItem<String>(
-                      value: selectedFacVal,
-                      child: Text(selectedFacVal),
-                    );
-                  }).toList(),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please select a facility';
-                    }
-                  },
-                  value: selectedFacility,
-                  onChanged: facilityCallBack,
+                      ),
+                      hintText: 'Full Name',
+                      prefixIcon: Icon(Icons.person_2_outlined)),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            dateBox(context),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: TextFormField(
-                minLines: 1,
-                maxLines: 5,
-                controller: _notesController,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 2, color: Colors.greenAccent),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: 'Notes',
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 25.0, horizontal: 10.0),
-                    prefixIcon: Icon(Icons.notes)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shadowColor: Colors.greenAccent,
-                        side: BorderSide(color: primary),
-                        backgroundColor: primary,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0))),
-                    onPressed: submitButton,
-                    child: Text('Submit'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: DropdownButtonFormField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.local_hospital_outlined),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Colors.greenAccent),
+                          borderRadius: BorderRadius.circular(12),
+                        )),
+                    hint: Text('Facility'),
+                    isExpanded: true,
+                    items: facilities
+                        .map<DropdownMenuItem<String>>((String selectedFacVal) {
+                      return DropdownMenuItem<String>(
+                        value: selectedFacVal,
+                        child: Text(selectedFacVal),
+                      );
+                    }).toList(),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a facility';
+                      }
+                    },
+                    value: selectedFacility,
+                    onChanged: facilityCallBack,
                   ),
-                ],
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              dateBox(context),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+                  minLines: 1,
+                  maxLines: 5,
+                  controller: _notesController,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.greenAccent),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'Notes',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 25.0, horizontal: 10.0),
+                      prefixIcon: Icon(Icons.notes)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.greenAccent,
+                          side: BorderSide(color: primary),
+                          backgroundColor: primary,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0))),
+                      onPressed: submitButton,
+                      child: Text('Submit'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

3d8cb47793d6128c87e038393a99be9ccd3ec37b
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2023-04-29 21:30:26 +0300

    Update barcode.dart

--- mobile_app/lib/pages/barcode.dart
+++ mobile_app/lib/pages/barcode.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class BarCode extends StatelessWidget {
-  const BarCode({super.key});
+  const BarCode({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double cardHeight = size.height * 0.25;
+    final double qrSize = size.width * 0.6;
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             SizedBox(
-              height: 200,
+              height: cardHeight,
               child: Card(
                 clipBehavior: Clip.antiAlias,
                 shadowColor: Colors.greenAccent,
@@ -40,31 +44,31 @@ class BarCode extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 40,
+              height: size.height * 0.02,
             ),
             Center(
               child: Text(
                 'Medical ID', //Da el patient ID
-                style: TextStyle(fontSize: 30),
+                style: TextStyle(fontSize: size.width * 0.1),
               ),
             ),
             SizedBox(
-              height: 10,
+              height: size.height * 0.01,
             ),
             Center(
               child: Text(
                 '659485-9857498-122',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: size.width * 0.05),
               ),
             ),
             SizedBox(
-              height: 60,
+              height: size.height * 0.05,
             ),
             Center(
               child: QrImage(
                 data: 'https://jsonplaceholder.typicode.com/posts/4',
                 version: QrVersions.auto,
-                size: 300.0,
+                size: qrSize,
               ),
             ),
           ],

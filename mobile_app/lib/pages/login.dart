56e80a01007b3f3537f87e106586a2c920ec4761
ahmedosama8 <ahmedosamaa1234@gmail.com>
2023-05-25 16:51:28 +0300

    Update login.dart

--- mobile_app/lib/pages/login.dart
+++ mobile_app/lib/pages/login.dart
@@ -115,152 +115,157 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // image
-                  Image.asset(
-                    'assets/loggo.png',
-                    height: 100,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  // title
-                  Text(
-                    'SIGN IN',
-                    style: GoogleFonts.robotoCondensed(
-                        fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-
-                  // subtitle
-                  Text(
-                    'Welcome back! Nice to see you again',
-                    style: GoogleFonts.robotoCondensed(
-                      fontSize: 18,
+    return WillPopScope(
+           onWillPop: () async {
+        return false; // Returning false prevents the user from going back
+      },
+      child: Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // image
+                    Image.asset(
+                      'assets/loggo.png',
+                      height: 100,
                     ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-
-                  // userName Text field
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primary),
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextFormField(
-                          controller: usernameController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Username',
-                              icon: Icon(Icons.person_pin_rounded)),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please write your username ';
-                            } else if (value.length < 4) {
-                              return "Too short write your username";
-                            }
-                            return null;
-                          },
-                        ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // title
+                    Text(
+                      'SIGN IN',
+                      style: GoogleFonts.robotoCondensed(
+                          fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+    
+                    // subtitle
+                    Text(
+                      'Welcome back! Nice to see you again',
+                      style: GoogleFonts.robotoCondensed(
+                        fontSize: 18,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-
-                  // password text field
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primary),
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a password ';
-                            }
-                            return null;
-                          },
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Password',
-                            icon: Icon(Icons.lock),
+                    SizedBox(
+                      height: 50,
+                    ),
+    
+                    // userName Text field
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: primary),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: TextFormField(
+                            controller: usernameController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Username',
+                                icon: Icon(Icons.person_pin_rounded)),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please write your username ';
+                              } else if (value.length < 4) {
+                                return "Too short write your username";
+                              }
+                              return null;
+                            },
                           ),
                         ),
                       ),
                     ),
-                  ),
-
-                  SizedBox(
-                    height: 15,
-                  ),
-
-                  // sign in button
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: GestureDetector(
-                      onTap:_loginButton,
+                    SizedBox(
+                      height: 10,
+                    ),
+    
+                    // password text field
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
                       child: Container(
-                        padding: EdgeInsets.all(16),
                         decoration: BoxDecoration(
-                          color: Color(0XFF66CA98),
-                          borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: primary),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a password ';
+                              }
+                              return null;
+                            },
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Password',
+                              icon: Icon(Icons.lock),
+                            ),
+                          ),
                         ),
-                        child: Center(
-                            child: Text(
-                          'Sign in',
-                          style: GoogleFonts.robotoCondensed(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        )),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-
-                  // sign up text
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Not yet a member? ',
-                          style: GoogleFonts.robotoCondensed(
-                              fontWeight: FontWeight.bold)),
-                      GestureDetector(
-                        onTap: openSignUpScreen,
-                        child: Text(
-                          ' Sign up now',
-                          style: GoogleFonts.robotoCondensed(
-                              color: Color(0XFF66CA98),
-                              fontWeight: FontWeight.bold),
+    
+                    SizedBox(
+                      height: 15,
+                    ),
+    
+                    // sign in button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: GestureDetector(
+                        onTap:_loginButton,
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0XFF66CA98),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Center(
+                              child: Text(
+                            'Sign in',
+                            style: GoogleFonts.robotoCondensed(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          )),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+    
+                    // sign up text
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Not yet a member? ',
+                            style: GoogleFonts.robotoCondensed(
+                                fontWeight: FontWeight.bold)),
+                        GestureDetector(
+                          onTap: openSignUpScreen,
+                          child: Text(
+                            ' Sign up now',
+                            style: GoogleFonts.robotoCondensed(
+                                color: Color(0XFF66CA98),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),

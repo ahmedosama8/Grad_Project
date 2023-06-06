0178dd82619eb8f057224302427b81894c7de465
ahmedosama8 <ahmedosamaa1234@gmail.com>
2023-06-06 11:38:21 +0300

    ...

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(DiagnosisSeed::class);
+        // $this->call(DiagnosisSeed::class);
         $this->call(PharmacySeed::class);
         // \App\Models\User::factory(10)->create();
 

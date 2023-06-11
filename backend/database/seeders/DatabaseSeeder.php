e6f05587eccb20bfd98288ad75158939f06df7bc
Omar Abuelfadl <55900407+Abuelfadl@users.noreply.github.com>
2023-06-11 15:26:18 +0300

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,8 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(DiagnosisSeed::class);
+        $this->call(DiagnosisSeed::class);
         $this->call(PharmacySeed::class);
+        $this->call(EntitySeed::class)
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
